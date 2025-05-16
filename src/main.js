5694642909af894f473ff39298a78bab3b6ccb93
mariohood <manhattan199@hotmail.com>
2025-05-16 20:14:41 -0300

    335 Agregando Firebase, Habilitando Storage

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+// Fireabase
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './config/firebase'
+
 import { plugin, defaultConfig } from '@formkit/vue'
 import config from '../formkit.config'
 
@@ -10,6 +15,11 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()],
+})
 app.use(plugin, defaultConfig(config))
 app.use(router)
 
